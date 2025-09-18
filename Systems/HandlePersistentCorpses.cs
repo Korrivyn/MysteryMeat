8cddf66cc8f4aec3d93bb1315227b0acefcf7207
Korrivyn <53236304+Korrivyn@users.noreply.github.com>
2025-09-18 17:32:20 -0500

    Wrong file replaced, ECB for persistent corpses.

--- Systems/HandlePersistentCorpses.cs
+++ Systems/HandlePersistentCorpses.cs
@@ -9,7 +9,7 @@ using Unity.Entities;
 namespace KitchenMysteryMeat.Systems
 {
     [UpdateInGroup(typeof(SimulationSystemGroup))]
-    public partial class HandleOvernightCorpses : SystemBase, IModSystem
+    public partial class HandlePersistentCorpses : SystemBase, IModSystem
     {
         private EndSimulationEntityCommandBufferSystem _endEcbSystem;
 
@@ -21,40 +21,62 @@ namespace KitchenMysteryMeat.Systems
 
         protected override void OnUpdate()
         {
+            if (!HasStatus((RestaurantStatus)VariousUtils.GetID("persistentcorpses")))
+                return;
+
             var ecb = _endEcbSystem.CreateCommandBuffer().AsParallelWriter();
 
-            bool persistent = HasStatus((RestaurantStatus)VariousUtils.GetID("persistentcorpses"));
+            var gameData = GameData.Main;
 
             Entities
-                .WithName("HandleOvernightCorpses_Safe")
+                .WithName("HandlePersistentCorpses_Safe")
                 .WithAll<CIllegalSight>()
-                .ForEach((Entity entity, int entityInQueryIndex) =>
+                .ForEach((Entity entity, int entityInQueryIndex, in CIllegalSight illegalSight) =>
                 {
+                    // Skip if TurnIntoOnDayStart doesn’t resolve to an appliance or item
+                    if (!gameData.TryGet(illegalSight.TurnIntoOnDayStart, out Appliance _, false) &&
+                        !gameData.TryGet(illegalSight.TurnIntoOnDayStart, out Item _, false))
+                        return;
+
+                    // ----- ITEM CASE -----
                     if (HasComponent<CItem>(entity))
                     {
-                        if (persistent)
+                        if (HasComponent<CHeldBy>(entity) &&
+                            RequireBuffer<CHeldBy>(entity, out var holder) &&
+                            !HasComponent<CPreservesContentsOvernight>(holder.Entity))
                         {
-                            if (!HasComponent<CPreservedOvernight>(entity))
-                                ecb.AddComponent<CPreservedOvernight>(entityInQueryIndex, entity);
-                        }
-                        else
-                        {
-                            if (HasComponent<CPreservedOvernight>(entity))
-                                ecb.RemoveComponent<CPreservedOvernight>(entityInQueryIndex, entity);
+                            ecb.AddComponent(entityInQueryIndex, entity, new CChangeItemType
+                            {
+                                NewID = illegalSight.TurnIntoOnDayStart,
+                            });
+
+                            if (HasComponent<CSplittableItem>(entity))
+                            {
+                                var split = GetComponent<CSplittableItem>(entity);
+                                ecb.AddComponent(entityInQueryIndex, entity, new CPersistPortions
+                                {
+                                    RemainingCount = split.RemainingCount,
+                                    TotalCount = split.TotalCount,
+                                });
+                            }
                         }
                     }
-
-                    if (HasComponent<CAppliance>(entity))
+                    // ----- APPLIANCE CASE -----
+                    else if (HasComponent<CAppliance>(entity))
                     {
-                        if (persistent)
+                        if (HasComponent<CPosition>(entity))
                         {
-                            if (HasComponent<CDestroyApplianceAtNight>(entity))
-                                ecb.RemoveComponent<CDestroyApplianceAtNight>(entityInQueryIndex, entity);
-                        }
-                        else
-                        {
-                            if (!HasComponent<CDestroyApplianceAtNight>(entity))
-                                ecb.AddComponent<CDestroyApplianceAtNight>(entityInQueryIndex, entity);
+                            var pos = GetComponent<CPosition>(entity);
+
+                            var newEntity = ecb.CreateEntity(entityInQueryIndex);
+                            ecb.AddComponent(entityInQueryIndex, newEntity, new CCreateAppliance
+                            {
+                                ID = illegalSight.TurnIntoOnDayStart,
+                                ForceLayer = OccupancyLayer.Ceiling
+                            });
+                            ecb.AddComponent(entityInQueryIndex, newEntity, pos);
+
+                            ecb.DestroyEntity(entityInQueryIndex, entity);
                         }
                     }
                 })
