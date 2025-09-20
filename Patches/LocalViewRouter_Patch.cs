d40538376ed95d27a81fbfc184a56a48d604f8b8
Korrivyn <korvic@live.ca>
2025-09-19 19:48:54 -0500

    Removed last errored reference. Now to fix the broken code as a result.

--- Patches/LocalViewRouter_Patch.cs
+++ Patches/LocalViewRouter_Patch.cs
@@ -8,7 +8,6 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
-using static UnityEngine.Rendering.VirtualTexturing.Debugging;
 
 namespace KitchenMysteryMeat.Patches
 {
