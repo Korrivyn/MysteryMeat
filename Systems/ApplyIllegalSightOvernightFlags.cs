6713495f6429d4d6f04ed94bdfbf0bab727dcb32
Korrivyn <53236304+Korrivyn@users.noreply.github.com>
2025-09-20 19:29:45 -0500

    Fix persistent corpses clearing from held appliances

--- Systems/ApplyIllegalSightOvernightFlags.cs
+++ Systems/ApplyIllegalSightOvernightFlags.cs
@@ -3,6 +3,7 @@ using KitchenData;
 using KitchenLib.Utils;
 using KitchenMods;
 using KitchenMysteryMeat.Components;
+using System.Collections.Generic;
 using Unity.Collections;
 using Unity.Entities;
 
@@ -11,6 +12,7 @@ namespace KitchenMysteryMeat.Systems
     public class ApplyIllegalSightOvernightFlags : GameSystemBase, IModSystem
     {
         private EntityQuery IllegalEntities;
+        private EntityQuery HolderPreservers;
 
         protected override void Initialise()
         {
@@ -20,34 +22,43 @@ namespace KitchenMysteryMeat.Systems
             {
                 All = new[] { ComponentType.ReadOnly<CIllegalSight>() }
             });
+
+            HolderPreservers = GetEntityQuery(new EntityQueryDesc
+            {
+                All = new[] { ComponentType.ReadOnly<CIllegalSightHolderPreserved>() }
+            });
         }
 
         protected override void OnUpdate()
         {
             bool persistent = HasStatus((RestaurantStatus)VariousUtils.GetID("persistentcorpses"));
 
-            using NativeArray<Entity> illegals = IllegalEntities.ToEntityArray(Allocator.Temp);
-            if (illegals.Length == 0)
-                return;
+            HashSet<Entity> holdersWithIllegalItems = persistent ? new HashSet<Entity>() : null;
 
+            using NativeArray<Entity> illegals = IllegalEntities.ToEntityArray(Allocator.Temp);
             for (int i = 0; i < illegals.Length; ++i)
             {
                 Entity entity = illegals[i];
                 CIllegalSight illegal = EntityManager.GetComponentData<CIllegalSight>(entity);
-
-                if (illegal.TurnIntoOnDayStart <= 0)
-                    continue;
+                bool canTurn = illegal.TurnIntoOnDayStart > 0;
+                bool hasPreservedFlag = EntityManager.HasComponent<CPreservedOvernight>(entity);
 
                 if (EntityManager.HasComponent<CItem>(entity))
                 {
                     if (persistent)
                     {
-                        if (!EntityManager.HasComponent<CPreservedOvernight>(entity))
+                        if (canTurn && !hasPreservedFlag)
                         {
                             EntityManager.AddComponentData(entity, new CPreservedOvernight());
+                            hasPreservedFlag = true;
+                        }
+
+                        if ((canTurn || hasPreservedFlag) && holdersWithIllegalItems != null)
+                        {
+                            HandleHeldItemPreservation(entity, holdersWithIllegalItems);
                         }
                     }
-                    else if (EntityManager.HasComponent<CPreservedOvernight>(entity))
+                    else if (canTurn && hasPreservedFlag)
                     {
                         EntityManager.RemoveComponent<CPreservedOvernight>(entity);
                     }
@@ -62,11 +73,80 @@ namespace KitchenMysteryMeat.Systems
                             EntityManager.RemoveComponent<CDestroyApplianceAtNight>(entity);
                         }
                     }
-                    else if (!EntityManager.HasComponent<CDestroyApplianceAtNight>(entity))
+                    else if (canTurn && !EntityManager.HasComponent<CDestroyApplianceAtNight>(entity))
                     {
                         EntityManager.AddComponentData(entity, new CDestroyApplianceAtNight());
                     }
                 }
+            }
+
+            CleanupHolderPreservers(persistent, holdersWithIllegalItems);
+        }
+
+        private void HandleHeldItemPreservation(Entity item, HashSet<Entity> holdersWithIllegalItems)
+        {
+            if (holdersWithIllegalItems == null)
+                return;
+
+            if (!EntityManager.HasComponent<CHeldBy>(item))
+                return;
+
+            CHeldBy heldBy = EntityManager.GetComponentData<CHeldBy>(item);
+            Entity holder = heldBy.Holder;
+
+            if (holder == Entity.Null || !EntityManager.HasComponent<CAppliance>(holder))
+                return;
+
+            holdersWithIllegalItems.Add(holder);
+
+            bool hadPreserver = EntityManager.HasComponent<CPreservesContentsOvernight>(holder);
+            if (!hadPreserver)
+            {
+                EntityManager.AddComponentData(holder, new CPreservesContentsOvernight());
+            }
+
+            if (EntityManager.HasComponent<CIllegalSightHolderPreserved>(holder))
+            {
+                if (!hadPreserver)
+                {
+                    CIllegalSightHolderPreserved marker = EntityManager.GetComponentData<CIllegalSightHolderPreserved>(holder);
+                    if (!marker.AddedPreserver)
+                    {
+                        marker.AddedPreserver = true;
+                        EntityManager.SetComponentData(holder, marker);
+                    }
+                }
+            }
+            else
+            {
+                EntityManager.AddComponentData(holder, new CIllegalSightHolderPreserved
+                {
+                    AddedPreserver = !hadPreserver
+                });
+            }
+        }
+
+        private void CleanupHolderPreservers(bool persistent, HashSet<Entity> holdersWithIllegalItems)
+        {
+            using NativeArray<Entity> holders = HolderPreservers.ToEntityArray(Allocator.Temp);
+            if (holders.Length == 0)
+                return;
+
+            for (int i = holders.Length - 1; i >= 0; --i)
+            {
+                Entity holder = holders[i];
+
+                if (persistent && holdersWithIllegalItems != null && holdersWithIllegalItems.Contains(holder))
+                    continue;
+
+                CIllegalSightHolderPreserved marker = EntityManager.GetComponentData<CIllegalSightHolderPreserved>(holder);
+
+                if (marker.AddedPreserver && EntityManager.HasComponent<CPreservesContentsOvernight>(holder))
+                {
+                    EntityManager.RemoveComponent<CPreservesContentsOvernight>(holder);
+                }
+
+                EntityManager.RemoveComponent<CIllegalSightHolderPreserved>(holder);
             }
         }
     }
