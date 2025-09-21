d627ba92cfa42cf54146e821554428f3d997bfa6
Korrivyn <53236304+Korrivyn@users.noreply.github.com>
2025-09-21 15:51:48 -0500

    Process appliance surfaces when rotting corpses

--- Systems/ApplyIllegalSightEffects.cs
+++ Systems/ApplyIllegalSightEffects.cs
@@ -7,6 +7,7 @@ using Kitchen;
 using KitchenMods;
 using KitchenMysteryMeat.Components;
 using KitchenMysteryMeat.Systems.Effects;
+using System.Collections.Generic;
 using Unity.Collections;
 using Unity.Entities;
 
@@ -24,26 +25,63 @@ namespace KitchenMysteryMeat.Systems
 
             using (NativeArray<Entity> illegals = query.ToEntityArray(Allocator.Temp))
             {
-                if (illegals.Length > 0)
+                if (illegals.Length == 0)
+                    return;
+
+                // Create an EntityContext backed by the project's EntityManager
+                EntityContext ctx = new EntityContext(EntityManager);
+                HashSet<Entity> processedItems = new HashSet<Entity>();
+
+                for (int i = illegals.Length - 1; i >= 0; --i)
                 {
-                    // Create an EntityContext backed by the project's EntityManager
-                    EntityContext ctx = new EntityContext(EntityManager);
+                    Entity e = illegals[i];
 
-                    for (int i = illegals.Length - 1; i >= 0; --i)
+                    if (ctx.Has<CItem>(e))
                     {
-                        Entity e = illegals[i];
-
-                        if (ctx.Has<CItem>(e))
+                        if (processedItems.Add(e))
                         {
                             CorpseEffects.TransformCorpse(ctx, e);
                         }
-                        else if (ctx.Has<CAppliance>(e))
-                        {
-                            CorpseEffects.ReplaceWithAppliance(ctx, e);
-                        }
+                    }
+                    else if (ctx.Has<CAppliance>(e))
+                    {
+                        ProcessApplianceSurfaceContents(ctx, e, processedItems);
+                        CorpseEffects.ReplaceWithAppliance(ctx, e);
                     }
                 }
             }
+        }
+
+        private void ProcessApplianceSurfaceContents(EntityContext ctx, Entity appliance, HashSet<Entity> processedItems)
+        {
+            if (processedItems == null)
+                return;
+
+            if (EntityManager.HasComponent<CItemHolder>(appliance))
+            {
+                CItemHolder holder = EntityManager.GetComponentData<CItemHolder>(appliance);
+                TryTransformHeldEntity(ctx, holder.HeldItem, processedItems);
+            }
+
+            if (EntityManager.HasBuffer<CItemStored>(appliance))
+            {
+                DynamicBuffer<CItemStored> storedItems = EntityManager.GetBuffer<CItemStored>(appliance);
+                for (int i = 0; i < storedItems.Length; i++)
+                {
+                    TryTransformHeldEntity(ctx, storedItems[i].StoredItem, processedItems);
+                }
+            }
+        }
+
+        private void TryTransformHeldEntity(EntityContext ctx, Entity heldItem, HashSet<Entity> processedItems)
+        {
+            if (heldItem == Entity.Null || !EntityManager.Exists(heldItem))
+                return;
+
+            if (!processedItems.Add(heldItem))
+                return;
+
+            CorpseEffects.TransformCorpse(ctx, heldItem);
         }
     }
 }
